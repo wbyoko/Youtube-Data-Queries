df7fed866fd6d8654e10f5b33ef96c1775f84eb8
William Bagayoko <wbagayoko@gmail.com>
2015-07-04 22:41:33 -0500

    try firefox

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -3,7 +3,7 @@ exports.config = {
   seleniumServerJar: '../node_modules/selenium-standalone-jar/bin/selenium-server-standalone-2.45.0.jar',
 
   capabilities: {
-    'browserName': 'phantomjs'
+    'browserName': 'firefox'
   },
 
   jasmineNodeOpts: {
