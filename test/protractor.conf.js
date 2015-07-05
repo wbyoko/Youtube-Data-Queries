ccea5bae28bbed2cf53f345d6edc2ec953670851
William Bagayoko <wbagayoko@gmail.com>
2015-07-04 22:37:25 -0500

    update tests

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -3,9 +3,7 @@ exports.config = {
   seleniumServerJar: '../node_modules/selenium-standalone-jar/bin/selenium-server-standalone-2.45.0.jar',
 
   capabilities: {
-    'browserName': 'phantomjs',
-    'phantomjs.binary.path': require('phantomjs').path,
-    'phantomjs.ghostdriver.cli.args': ['--loglevel=DEBUG']
+    'browserName': 'phantomjs'
   },
 
   jasmineNodeOpts: {
